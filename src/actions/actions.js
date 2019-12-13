a272e065f2099ca3af8a6852ea8876507154a185
nrico90 <nrico90@gmail.com>
2019-12-13 14:37:01 +0100

    actions

---
+++ src/actions/actions.js
@@ -0,0 +1,15 @@
+const ALL_MESSAGES = "ALL_MESSAGES";
+export function allMessages(messages) {
+  return {
+    type: ALL_MESSAGES,
+    payload: messages
+  };
+}
+const NEW_MESSAGE = "NEW_MESSAGE";
+
+export function newMessage(message) {
+  return {
+    type: NEW_MESSAGE,
+    payload: message
+  };
+}
